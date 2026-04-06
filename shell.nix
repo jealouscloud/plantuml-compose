9edff5a619fce830ecbd5279f5dacceed1e5d562
jealouscloud <github@noaha.org>
2026-04-05 22:22:13 -0400

    shell.nix represents latest

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/6201e203d09599479a3b3450ed24fa81537ebc4e.tar.gz") {} }:  
 pkgs.mkShell {
   buildInputs = [
     pkgs.plantuml
+    pkgs.playwright-mcp
   ];
 }
